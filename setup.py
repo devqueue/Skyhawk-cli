774e7177ec06cca123060d35dd07cda80bd6cbe4
smokedpirate <smokedpirate@protonmail.com>
2020-10-22 19:01:24 +0300

    All set for testing

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 setup(
-    name='skyhawk-cli',
+    name='skyhawkcli',
     version='1.0',
-    packages=['skyhawk-cli'],
+    packages=['skyhawkcli'],
     entry_points={
         'console_scripts': [
             'skyhawkcli = skyhawkcli.__main__:main'
