7286ae13ed8583105c047ef4104e9f96628e27a0
smokedpirate <smokedpirate@protonmail.com>
2020-10-22 18:53:41 +0300

    Default code Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='pycli',
+    version='0.1.0',
+    packages=['pycli'],
+    entry_points={
+        'console_scripts': [
+            'pycli = pycli.__main__:main'
+        ]
+    })
