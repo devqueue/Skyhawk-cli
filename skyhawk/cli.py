8474c17da2d80803e8e659f3b8a6544118b1ea73
smokedpirate <smokedpirate@protonmail.com>
2020-10-23 15:16:04 +0300

    complex class added

--- skyhawk/cli.py
+++ skyhawk/cli.py
@@ -1,5 +1,25 @@
+import os
 import click
 
-@click.command()
+class ComplexCLI(click.MultiCommand):
+    def list_commands(self, ctx):
+        commands = []
+        commands_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "commands"))
+        for filename in os.listdir(commands_folder):
+            if filename.endswith(".py") and filename.startswith("cmd_"):
+                commands.append(filename.replace("cmd_", "").replace(".py", ""))
+
+        commands.sort()
+        return commands
+
+    def get_command(self, ctx, name):
+        try:
+            mod = __import__(f"skyhawk.commands.cmd_{name}", None, None, ["cli"])
+        except ImportError:
+            return
+        return mod.cli
+
+@click.command(cls=ComplexCLI)
 def cli():
-    click.echo('hello world')
+    """Welcome to EVE! Auto-attandance cli utility tool!"""
+    pass
