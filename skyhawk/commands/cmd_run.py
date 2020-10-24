739382f936c8f519b9de327c45fb2777c0eb15ac
smokedpirate <smokedpirate@protonmail.com>
2020-10-24 17:22:19 +0300

    Better naming

--- skyhawk/commands/cmd_run.py
+++ skyhawk/commands/cmd_run.py
@@ -1,5 +1,5 @@
 import click
-from skyhawk.services import main
+from skyhawk.services import detector
 
 
 class Context:
@@ -10,7 +10,7 @@ class Context:
 @click.command()
 def cli():
     '''Start running skyhawk for face recognition'''
-    result = main.run()
+    result = detector.run()
     return result
 
 
