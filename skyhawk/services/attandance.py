5c34a558e8da38ae3d6542a0b87d27d401dea037
smokedpirate <smokedpirate@protonmail.com>
2020-10-29 11:25:57 +0300

    Attendance view added

--- skyhawk/services/attandance.py
+++ skyhawk/services/attandance.py
@@ -1,12 +1,39 @@
-#!venv/bin/python3.8
+from prettytable import PrettyTable
+from prettytable import from_csv
+
+
 def view():
-    ""
-    with open("skyhawk/services/Attendance.csv", "r") as file:
-        lines = file.readlines()
-        #print(lines)
-        for line in lines:
-            #print(line)
-            attributes = line.split(",")
-            #print(attributes)
-            for attribute in attributes:
-                print(attribute, end = "")
+    with open('skyhawk/services/Attendance.csv', 'r') as inf:
+        table = from_csv(inf)
+        print(table)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # with open("skyhawk/services/Attendance.csv", "r") as file:
+    #     lines = file.readlines()
+    #     #print(lines)
+    #     for line in lines:
+    #         #print(line)
+    #         attributes = line.split(",")
+    #         #print(attributes)
+    #         for attribute in attributes:
+    #             print(attribute, end = "")
+    #     print('\n')
