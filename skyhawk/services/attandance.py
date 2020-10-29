e9e3caa652c0d059a5a6b716c44e95ea2e3aa437
smokedpirate <smokedpirate@protonmail.com>
2020-10-29 11:53:40 +0300

    Attandance view added

--- skyhawk/services/attandance.py
+++ skyhawk/services/attandance.py
@@ -7,33 +7,3 @@ def view():
         table = from_csv(inf)
         print(table)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # with open("skyhawk/services/Attendance.csv", "r") as file:
-    #     lines = file.readlines()
-    #     #print(lines)
-    #     for line in lines:
-    #         #print(line)
-    #         attributes = line.split(",")
-    #         #print(attributes)
-    #         for attribute in attributes:
-    #             print(attribute, end = "")
-    #     print('\n')
