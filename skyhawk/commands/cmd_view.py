90af47f024ccf453940da2f666a1b6170cf80fca
smokedpirate <smokedpirate@protonmail.com>
2020-10-29 21:15:36 +0300

    View command added

--- skyhawk/commands/cmd_view.py
+++ skyhawk/commands/cmd_view.py
@@ -2,11 +2,29 @@ import click
 from skyhawk.services import view
 
 
+class Context:
+    def __init__(self):
+        pass
 
 
-@click.command()
-def cli():
+@click.group()
+@click.pass_context
+def cli(ctx):
+	'''View the App data'''
+	ctx.obj = Context()
+	
+
+@cli.command()
+@click.pass_context
+def attandance(ctx):
 	'''View the attandance'''
-	result = view.View()
+	result = view.Viewer.view()
 	return result
 
+
+@cli.command()
+@click.pass_context
+def users(ctx):
+	'''View registered users'''
+	result = view.Viewer.users()
+	return result
