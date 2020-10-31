8e317ba4375e1ad00a107dc93d9cf79cd9b317b5
smokedpirate <smokedpirate@protonmail.com>
2020-10-31 18:52:50 +0300

    classifiers added

--- setup.py
+++ setup.py
@@ -1,25 +1,36 @@
 from setuptools import setup, find_packages
 
+
 def read_requirements():
     with open('requirements.txt') as req:
         content = req.read()
         requirements = content.split('\n')
     return requirements
 
+
 classifiers = [
+    'Topic:: Security',
     'Development Status :: 4 - Beta',
+    'Environment:: Console',
+    'License :: Mozilla Public License 2.0',
     'Intended Audience :: End Users/Desktop',
     'Operating System :: OS Independent',
-    'License :: OSI Approved :: Mozilla Public License 2.0',
-    'Programming Language :: Python :: 3',
-    'Topic:: Security'
+    'Programming Language :: Python :: 3.7'
 ]
+
+with open('README.md') as rm:
+    RM_desc = rm.read()
+
+with open('CHANGELOG.md') as cl:
+    CL_desc = cl.read
+
 
 setup(
     name='skyhawk',
     version='0.1.0',
     description='Skyhawk is a CLI tool that can run on any device with a camera to recognize faces. It built with open-cv & python',
-    Long_description=open('README.md').read +'\n\n' + open('CHANGELOG.md').read(),
+    Long_description=RM_desc + '\n\n' + CL_desc,
+    long_description_content_type="text/markdown",
     url='https://github.com/smokedpirate/Skyhawk-cli.git',
     author='Haziq Sayyed',
     author_email='haziq.sayyed@gmail.com',
@@ -28,6 +39,7 @@ setup(
     packages=find_packages(),
     include_package_dat=True,
     install_requires=read_requirements(),
+    python_requires='>=3.8',
     entry_points={
         'console_scripts': [
             'skyhawk = skyhawk.cli:cli'
