5391a374a10c479d14c55e969cb0dbedfeea8ba6
smokedpirate <smokedpirate@protonmail.com>
2020-11-01 14:36:49 +0300

    configs moved to test

---
+++ test/configure.py
@@ -0,0 +1,15 @@
+import os
+
+# class configdata():
+#     def __init__():
+#         pass
+
+
+#     def imgdir(img):
+        
+
+
+#     def bindir():
+#         pass
+
+print(os.path.abspath("."))
