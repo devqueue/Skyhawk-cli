a21cbe3eddb5719014f888e91e512cf51b293341
smokedpirate <smokedpirate@protonmail.com>
2020-11-01 16:44:17 +0300

    Version 0.0.4 added

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open('ChangeLog.md') as cl:
 
 setup(
     name='skyhawk',
-    version='0.0.3',
+    version='0.0.4',
     description='Skyhawk is a CLI tool that can run on any device with a camera to recognize faces. It built with open-cv & python',
     Long_description=RM_desc + '\n\n' + CL_desc,
     long_description_content_type="text/markdown",
