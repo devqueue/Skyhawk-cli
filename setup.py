8b96629c4ae3a0c1affbe79db6bcd3caa3a5ec79
devqueue <haziq.sayyed@gmail.com>
2020-11-12 15:16:23 +0300

    functions defined for reading long description

--- setup.py
+++ setup.py
@@ -18,20 +18,22 @@ classifiers = [
     'Programming Language :: Python :: 3.7'
 ]
 
-with open('README.md') as rm:
-    RM_desc = rm.read()
-    RM_desc = str(RM_desc)
+def readme():
+    with open('README.md') as rm:
+        RM_desc = rm.read()
+        RM_desc = str(RM_desc)
 
-with open('ChangeLog.md') as cl:
-    CL_desc = cl.read
-    CL_desc = str(CL_desc)
+def changelog():
+    with open('ChangeLog.md') as cl:
+        CL_desc = cl.read
+        CL_desc = str(CL_desc)
 
 
 setup(
     name='skyhawk',
     version='0.0.5',
     description='Skyhawk is a CLI tool that can run on any device with a camera to recognize faces. It built with open-cv & python',
-    Long_description=RM_desc + '\n\n' + CL_desc,
+    Long_description=readme() + '\n\n' + changelog(),
     long_description_content_type="text/markdown",
     url='https://github.com/devqueue/Skyhawk-cli.git',
     author='Haziq Sayyed',
